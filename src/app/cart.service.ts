9815a5833f4e92363fb909aa9f94e27632cd5651
bdomokos74 <bdomokos74@gmail.com>
2022-02-06 17:48:29 +0100

    Shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -6,7 +7,7 @@ import { Product } from './products';
 })
 export class CartService {
   items : Product[] = [];
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,5 +20,11 @@ export class CartService {
   clearChart(): Product[] {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.httpClient.get<{type: string, price: number}[]>(
+      '/assets/shipping.json'
+    );
   }
 }
